a039e73d5def4cd58c1491e09d3323eea3bd46d6
NoraAhmedYousef <nora.ahmed.clubly@gmail.com>
2026-04-21 04:22:27 +0200

    Add guests model ,dtos and controller

--- Back-end/obj/Debug/net8.0/Clubly.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Clubly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9277394bec5b4d639bbde7cb0df4ad25c20c5b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a129430b405951317fa81902e892ce7c56c5dd27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
