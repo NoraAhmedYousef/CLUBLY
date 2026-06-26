8dd0cad2dbd07c02691bc71315182c0df0b69e06
NoraAhmedYousef <nora.ahmed.clubly@gmail.com>
2026-06-27 01:10:34 +0300

    Create models dtos services and reposatories

--- Back-end/obj/Debug/net8.0/Clubly.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Clubly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81478ede58a8fcbd6fd77f51347ede52bc994b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d27dad0e351899cf1579342a1a206eaa196a9c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clubly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
